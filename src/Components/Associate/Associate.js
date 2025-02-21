4669f5d31e40a1fb99ef90a688425e3ab2af3213
davidmicah123 <micahdanielsd@gmail.com>
2025-02-21 19:23:35 +0100

    Updated website

--- src/Components/Associate/Associate.js
+++ src/Components/Associate/Associate.js
@@ -3,6 +3,7 @@ import classes from "./Associate.module.css"
 import soccerverse from "../../Assets/Images/soccerverse.png"
 import travelsmart from "../../Assets/Images/travelsmart.png"
 import foodmania from "../../Assets/Images/foodmania.png"
+import huddle from "../../Assets/Images/huddle.png";
 
 const Associate = () => {
   return (
@@ -11,7 +12,7 @@ const Associate = () => {
 
       <div className={classes.asscoiate_logo_wrapper}>
         <img src={foodmania} alt="" />
-        <img src={soccerverse} alt="" />
+        <img src={huddle} alt="" />
         <img src={travelsmart} alt="" />
         <img
           src="https://techpath-insights.netlify.app/static/media/8.7aae5ae3b90d4171aa28.png"
